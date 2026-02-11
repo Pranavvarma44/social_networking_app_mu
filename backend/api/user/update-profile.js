2f8a1c84ff480f3e222af08de3bace1a72d2163a
pranavrudraraju <pranavrudraraju0@gmail.com>
2026-02-11 14:15:26 +0530

    added profile routes

--- backend/api/user/update-profile.js
+++ backend/api/user/update-profile.js
@@ -1,6 +1,6 @@
 import connectDB from "../../lib/db";
 import User from "../../models/User";
-import  requireAuth  from "../../lib/auth";
+import  {requireAuth } from "../../lib/auth";
 
 export default async function handler(req,res){
     if(req.method!=="PUT"){
