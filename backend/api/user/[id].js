f18f1b92a4c09937824f7b4542a654c840b6aa46
pranavrudraraju <pranavrudraraju0@gmail.com>
2026-03-01 16:03:19 +0530

    frontend test

--- backend/api/user/[id].js
+++ backend/api/user/[id].js
@@ -2,6 +2,15 @@ import connectDB from "../../lib/db.js";
 import User from "../../models/User.js";
 
 export default async function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const { id } = req.query;
 
   await connectDB();
