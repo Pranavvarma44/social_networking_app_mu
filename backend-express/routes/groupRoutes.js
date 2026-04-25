bb1addd1382139908e355af1460ce087b0374306
pranavrudraraju <pranavrudraraju0@gmail.com>
2026-04-25 16:59:35 +0530

    modified messages

--- backend-express/routes/groupRoutes.js
+++ backend-express/routes/groupRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Group from "../models/Group.js";
-import requireAuth from "../middleware/requireAuth.js";
+import {requireAuth} from "../middleware/requireAuth.js";
 
 const router = express.Router();
 
--- backend-express/routes/messageRoutes.js
+++ backend-express/routes/messageRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Message from "../models/Message.js";
-import  requireAuth from "../middleware/requireAuth.js";
+import  {requireAuth} from "../middleware/requireAuth.js";
 
 const router = express.Router();
 
