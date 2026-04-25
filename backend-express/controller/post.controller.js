d2e9cd763696b660ff16dac9de9064e86e771838
pradhs827 <pradyumnagunupuru8@gmail.com>
2026-04-25 16:38:14 +0530

    your changes

---
+++ backend-express/controller/post.controller.js
@@ -0,0 +1,51 @@
+import Post from "../../models/postModel.js";
+
+export const createPost = async (req, res, next) => {
+  try {
+    const { content, image } = req.body;
+
+    const post = await Post.create({
+      author: req.user.id,
+      content,
+      image,
+    });
+
+    res.status(201).json(post);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get all posts (basic feed)
+export const getPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find()
+      .populate("author", "name email")
+      .sort({ createdAt: -1 });
+
+    res.json(posts);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete post
+export const deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    if (post.author.toString() !== req.user.id) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+
+    await post.deleteOne();
+
+    res.json({ message: "Post deleted" });
+  } catch (err) {
+    next(err);
+  }
+};
