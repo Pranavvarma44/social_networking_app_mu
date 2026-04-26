f15ff1fc43668f4c4bcced4b45bf4128a80a85c3
pranavrudraraju <pranavrudraraju0@gmail.com>
2026-04-26 11:11:08 +0530

    modifications

--- backend-express/routes/postRoutes.js
+++ backend-express/routes/postRoutes.js
@@ -22,34 +22,26 @@ const router = express.Router();
 ========================= */
 
 // Create post
-router.post("/posts", requireAuth, createPost);
+router.post("/", requireAuth, createPost);
 
-// Get all posts (feed)
-router.get("/posts", requireAuth, getPosts);
+// GET POSTS
 
-// Delete post
-router.delete("/posts/:postId", requireAuth, deletePost);
+router.get("/", requireAuth, getPosts);
 
+// DELETE POST
 
-/* =========================
-   COMMENT ROUTES
-========================= */
+router.delete("/:postId", requireAuth, deletePost);
 
-// Add comment to a post
-router.post("/posts/:postId/comments", requireAuth, addComment);
+// COMMENTS
 
-// Get comments of a post
-router.get("/posts/:postId/comments", requireAuth, getComments);
+router.post("/:postId/comments", requireAuth, addComment);
 
-// Delete a comment
+router.get("/:postId/comments", requireAuth, getComments);
+
 router.delete("/comments/:commentId", requireAuth, deleteComment);
 
+// LIKE
 
-/* =========================
-   LIKE ROUTES
-========================= */
-
-// Like / Unlike toggle
-router.post("/posts/:postId/like", requireAuth, toggleLike);
+router.post("/:postId/like", requireAuth, toggleLike);
 
 export default router;
