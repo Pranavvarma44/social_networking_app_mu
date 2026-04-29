a55ae1e603e9337dc25fea24eb9664e7f4a2cd92
vaideesh_k <vaideeshk.acad@gmail.com>
2026-04-29 18:50:35 +0530

    Added frontend code

--- frontend/src/dashboard/Dashboard.tsx
+++
@@ -1,7 +0,0 @@
-export default function Dashboard() {
-    return (
-      <div className="flex min-h-screen items-center justify-center text-3xl font-bold">
-        Welcome to Dashboard 🚀
-      </div>
-    )
-  }
