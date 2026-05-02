bb1e975649e8e521bbf7d239c6c373987913a5e0
pranavrudraraju <pranavrudraraju0@gmail.com>
2026-05-02 12:14:03 +0530

    only following user or followers will be visible in messages

--- backend-express/routes/userRoutes.js
+++ backend-express/routes/userRoutes.js
@@ -11,6 +11,24 @@ router.get("/me", requireAuth, async (req, res) => {
   });
 });
 
+router.get("/chat-user",requireAuth,async(req,res)=>{
+  try{
+    const user=await User.findById(req.user._id).populate("following","name email").populate("followers","name email");
+    const usermap=new Map();
+    user.following.forEach(u=>usermap.set(u._id.toString(),u));
+    user.followers.forEach(u=>usermap.set(u._id.toString(),u));
+
+    const chatUsers=Array.from(usermap.values());
+    res.json(chatUsers);
+
+
+
+  }catch(error){
+    console.error("CHAT USERS ERROR:", err);
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+})
+
 router.get("/:id", requireAuth, async (req, res) => {
     try {
       const user = await User.findById(req.params.id)
@@ -61,8 +79,6 @@ router.get("/", async (req, res) => {
       res.status(500).json({ error: "Server error" });
     }
   });
-
-
 
 router.post("/:id/follow", requireAuth, async (req, res) => {
     try {
